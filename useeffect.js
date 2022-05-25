1ea8c5683938780bc622c017e90225433d6c87b8
yagnikkardani <57307913+yagnikkardani@users.noreply.github.com>
2022-05-25 15:31:48 +1000

    Update useeffect.js

--- useeffect.js
+++ useeffect.js
@@ -12,8 +12,17 @@ function SideEffects() {
   useEffect( () => setEffectType("Only one time on Component Mount (On First Render of this component)"), [] );
   useEffect( () => setEffectType("On Component Mount (On first render of this component) / On Count Update"), [ count ] );
   useEffect( () => setEffectType("On Component Mount (On first render of this component) / Update anything in this component") );
-  useEffect( () => () => setEffectType("On Component Mount (On first render of this component) / On count update / On component Dismount"), [ count ] );
-  useEffect( () => () => setEffectType("On Component Mount (On first render of this component) / Dismount"), [] );
+  useEffect( () => () => setEffectType("On Component Mount (On first render of this component) / On count update / On component Unmount"), [ count ] );
+  useEffect( () => () => setEffectType("On Component Mount (On first render of this component) / Unsmount"), [] );
+  
+  useEffect(() => {
+    let timer = setTimeout(() => {
+    setCount((count) => count + 1);
+  }, 5000);
+
+  return () => clearTimeout(timer) 
+  // This is a clean-up function of this useEffect
+  }, []);
 
   return (
     <div>
